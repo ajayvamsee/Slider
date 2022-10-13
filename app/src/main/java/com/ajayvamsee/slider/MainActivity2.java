4107c3b87b2ebab9cb66c03f7ddf0042503fe105
ajayvamsee24 <ajay.vamsee@techolution.com>
2022-10-13 15:13:58 +0530

    testing for switch button is set to clickable

--- app/src/main/java/com/ajayvamsee/slider/MainActivity2.java
+++ app/src/main/java/com/ajayvamsee/slider/MainActivity2.java
@@ -3,7 +3,11 @@ package com.ajayvamsee.slider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+
+import com.ajayvamsee.slider.databinding.ActivityMain2Binding;
 
 import java.net.NetworkInterface;
 import java.util.Collections;
@@ -11,15 +15,22 @@ import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private ActivityMain2Binding binding;
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-         textView= findViewById(R.id.textView1);
 
-         textView.setText(getMacAddr());
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setContentView(R.layout.activity_main2);
+        // textView= findViewById(R.id.textView1);
+
+         //textView.setText(getMacAddr());
+
+        loading();
     }
 
     public static String getMacAddr() {
@@ -48,5 +59,76 @@ public class MainActivity2 extends AppCompatActivity {
             ex.printStackTrace();
         }
         return "";
+    }
+
+    private void loading() {
+        binding.swOpenClose.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    binding.tvDoorStatus.setText(R.string.opened);
+                }else{
+                    binding.tvDoorStatus.setText(R.string.closed);
+                }
+
+            }
+        });
+
+        binding.swSettingClutch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    binding.swOpenClose.setClickable(true);
+                    binding.tvClutchStatus.setText(R.string.activated);
+                    binding.cvOprDisable.setVisibility(View.GONE);
+                    if(binding.clutchSwitch.isChecked()){
+                        binding.cvOprDisableInstall.setVisibility(View.GONE);
+                    }else{
+                        binding.cvOprDisableInstall.setVisibility(View.VISIBLE);
+                    }
+                }else {
+                    binding.swOpenClose.setClickable(false);
+                    binding.tvClutchStatus.setText(R.string.deactivated);
+                    binding.cvOprDisable.setVisibility(View.VISIBLE);
+                    if(binding.cvOprDisableInstall.getVisibility() == View.VISIBLE){
+                        binding.cvOprDisableInstall.setVisibility(View.GONE);
+                        binding.cvOprDisable.setVisibility(View.VISIBLE);
+                    }
+
+                }
+            }
+        });
+
+        // installation
+
+        binding.clutchSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    binding.swOpenClose.setClickable(true);
+                    binding.cvOprDisableInstall.setVisibility(View.GONE);
+                    if(binding.clutchSwitch.isChecked()){
+                        binding.cvOprDisable.setVisibility(View.GONE);
+                        if(binding.swSettingClutch.isChecked()){
+                            binding.cvOprDisable.setVisibility(View.GONE);
+                        }else{
+                            binding.cvOprDisable.setVisibility(View.VISIBLE);
+                        }
+                    }else{
+                        binding.cvOprDisable.setVisibility(View.GONE);
+                        binding.cvOprDisableInstall.setVisibility(View.VISIBLE);
+
+                    }
+                }else {
+                    binding.swOpenClose.setClickable(false);
+                    binding.cvOprDisableInstall.setVisibility(View.VISIBLE);
+                    if(binding.cvOprDisable.getVisibility() == View.VISIBLE){
+                        binding.cvOprDisableInstall.setVisibility(View.VISIBLE);
+                        binding.cvOprDisable.setVisibility(View.GONE);
+                    }
+
+                }
+            }
+        });
     }
 }
